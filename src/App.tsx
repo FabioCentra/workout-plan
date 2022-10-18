369420983b80336963b6588920cb7014482d9ee2
Fabio Centra <fabio.centra.dep@gmail.com>
2022-10-18 09:33:59 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+      errorElement:<div>Sorry, an unexpected error has occurred</div>
+    },
+    {
+      path: "*",
+      element: <div>Pagina non trovata</div>
+    }
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,19 @@
+import Box from '@mui/material/Box';
+import React from 'react';
+import '../App.css';
+
+function Home() {
+
+
+
+  return (
+
+    <>
+      <Box>
+
+      </Box>
+    </>
+  );
+}
+
+export default Home;
